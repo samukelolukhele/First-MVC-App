ba87c1d353dc083e741458743a2cd110bf5d98a5
samukelolukhele <samukelo.business@gmail.com>
2023-04-08 14:58:56 +0200

    Models initialized.

---
+++ Models/Owner.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace First_MVC_App.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Gym { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ Models/Review.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace First_MVC_App.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+    }
+}
