2a32826eae3b503f5935cd507d3e58ab94d6c7b6
samukelolukhele <samukelo.business@gmail.com>
2023-04-08 14:31:47 +0200

    Converted from MVC to WebAPI.

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace First_MVC_App.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
